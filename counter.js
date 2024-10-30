7b12762613857e86bbf2ca584fe48d8ed4323e5a
Ignat Insarov <kindaro@gmail.com>
2024-10-30 13:27:33 +0800

    Add styling and make it look nice.

--- counter.js
+++ counter.js
@@ -6,14 +6,14 @@ const Counter = function () {
         setState({ ...state, step: 2, counter: state.counter + 1 });
       else
         setTimeout(
-          () => setState({ ...state, opacity: state.opacity - 0.1 }),
+          () => setState({ ...state, opacity: state.opacity - 0.125 }),
           100,
         );
     } else if (state.step == 2) {
       if (state.opacity > 1) setState({ ...state, step: 0, opacity: 1 });
       else
         setTimeout(
-          () => setState({ ...state, opacity: state.opacity + 0.1 }),
+          () => setState({ ...state, opacity: state.opacity + 0.125 }),
           100,
         );
     }
@@ -21,14 +21,43 @@ const Counter = function () {
   if (state.step > 0) updateState();
 
   return (
-    <div>
-      <h1>counter</h1>
-      <button onClick={() => setState({ ...state, step: 1 })}>
-        one more please
-      </button>
-      <p style={{ opacity: state.opacity }}>{state.counter}</p>
-      <p>debug information: {JSON.stringify(state)}</p>
+    <div class="card">
+      <div class="card-body">
+        <h1 class="card-title">counter</h1>
+        <p class="card-text">
+          quantity:{" "}
+          <strong style={{ opacity: state.opacity }}>{state.counter}</strong>
+        </p>
+        <DictionaryTable dictionary={state} />
+        <button
+          class="btn btn-primary"
+          onClick={() => setState({ ...state, step: 1 })}
+        >
+          one more please
+        </button>
+      </div>
     </div>
+  );
+};
+
+const DictionaryTable = function ({ dictionary: dictionary }) {
+  return (
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">key</th>
+          <th scope="col">value</th>
+        </tr>
+      </thead>
+      <tbody class="table-group-divider">
+        {Object.entries(dictionary).map(([key, value]) => (
+          <tr>
+            <td class="col-6">{key}</td>
+            <td class="col-6">{value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
