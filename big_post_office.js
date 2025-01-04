a9a02ef0d96f58336aae3fca556a05f08b9a9069
Ignat Insarov <kindaro@gmail.com>
2025-01-04 22:52:44 +0700

    Add a big post office with a pigeon and a postman.

---
+++ big_post_office.js
@@ -0,0 +1,76 @@
+function Sender({ mail }) {
+  const [message, setMessage] = React.useState("");
+  return (
+    <div class="card m-3">
+      <div class="card-body">
+        <h5 class="card-header mb-3">sender</h5>
+        <input
+          class="form-control mb-3"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button class="btn btn-primary" onClick={() => mail(message)}>
+          send message
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Receiver({ mail }) {
+  return (
+    <div class="card m-3">
+      <div class="card-body">
+        <h5 class="card-header mb-3">receiver</h5>
+        <p>{mail}</p>
+      </div>
+    </div>
+  );
+}
+
+function Pigeon({ mail }) {
+  console.log(typeof mail);
+  return (
+    <div class="card m-3">
+      <div class="card-body">
+        <h5 class="card-header mb-3">pigeon</h5>
+        <Sender mail={mail} />
+      </div>
+    </div>
+  );
+}
+
+function Postman({ mail }) {
+  console.log(typeof mail);
+  return (
+    <div class="card m-3">
+      <div class="card-body">
+        <h5 class="card-header mb-3">postman</h5>
+        <Receiver mail={mail} />
+      </div>
+    </div>
+  );
+}
+
+function PostOffice() {
+  const [A, setA] = React.useState(undefined);
+  return (
+    <div class="card m-3">
+      <div class="card-body">
+        <h5 class="card-header mb-3">post office</h5>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <Pigeon mail={setA} />
+            </div>
+            <div class="col">
+              <Postman mail={A} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<PostOffice />, document.getElementById("big-post-office"));
