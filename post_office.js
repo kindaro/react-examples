c70ef70b86a04483bb06702ba20431c770b9a44a
Ignat Insarov <kindaro@gmail.com>
2025-01-04 22:16:21 +0700

    Add a post office example.

---
+++ post_office.js
@@ -0,0 +1,33 @@
+function Sender({ mail }) {
+  const [message, setMessage] = React.useState("");
+  return (
+    <div class="card m-3">
+      <div class="card-body">
+        <h5 class="card-header mb-3">sender</h5>
+        <input
+          class="form-control mb-3"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button class="btn btn-primary" onClick={() => mail(message)}>
+          send message
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function PostOffice() {
+  const [A, setA] = React.useState(undefined);
+  return (
+    <div class="card m-3">
+      <div class="card-body">
+        <h5 class="card-header mb-3">post office</h5>
+        <Sender mail={setA} />
+        <Receiver mail={A} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<PostOffice />, document.getElementById("post-office"));
